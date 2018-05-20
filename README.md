47a95853ccd93953018be12ab657da6a862a385b
aconfee <adamestela@gmail.com>
2018-05-19 20:01:26 -0500

    split out function

---
+++ README.md
@@ -0,0 +1,7 @@
+See serverless.yml for env settings.
+
+Deploy with `serverless deploy`
+
+Functions:
+
+- Get Kim's Instagram feed.
--- handler.js
+++ handler.js
@@ -1,41 +1,4 @@
 'use strict';
 
-const axios = require('axios');
+require('./instagramFeed.js');
 
-const INSTAGRAM_ACCESS_TOKEN = process.env.INSTAGRAM_ACCESS_TOKEN;
-if(!INSTAGRAM_ACCESS_TOKEN) {
-  console.error("Please set environment variable INSTAGRAM_ACCESS_TOKEN for instagram feed.");
-}
-
-module.exports.getInstagramFeed = (event, context, callback) => {
-  const count = 20;
-  const url = `https://api.instagram.com/v1/users/1381993170/media/recent/?access_token=${INSTAGRAM_ACCESS_TOKEN}&count=${count}`;
-
-  axios.get(url).then(response => {
-    const images = response.data.data.map(feedItem => {
-      return feedItem.images.low_resolution.url;
-    });
-
-    const successResponse = {
-      statusCode: 200,
-      headers: { "Access-Control-Allow-Origin": "http://www.kimgreenough.com" },
-      body: JSON.stringify({
-        response: images
-      }),
-    };
-
-    callback(null, successResponse);
-  })
-  .catch(error => {
-
-    const errorResponse = {
-      statusCode: 500,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: JSON.stringify({
-        error: error
-      }),
-    };
-
-    callback(null, errorResponse);
-  });
-};
